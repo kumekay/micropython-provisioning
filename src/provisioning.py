5f551f20885509f518123b09d4912c1069b70726
Sergei Silnov <po@kumekay.com>
2021-11-09 23:03:34 +0100

    Clean up provisioning code

--- src/provisioning.py
+++ src/provisioning.py
@@ -9,25 +9,29 @@ class Provisioning:
         self.debug = debug
 
     async def connect(self):
+        debug = self.debug
         try:
             creds = await self.store.read()
-            self.debug and print("Loaded previously saved credentials", dict(creds))
+            debug and print("Loaded previously saved credentials", dict(creds))
         except ValueError:
-            self.debug and print("Await credentials")
+            debug and print("Await credentials")
             creds = await self.transport()
             await self.store.write(**creds)
-            self.debug and print("Got credentials", dict(creds))
+            debug and print("Got credentials", dict(creds))
 
         return await self.connect_sta(creds["ssid"], creds["pw"])
 
     async def connect_sta(self, ssid, pw):
+        sleep_ms = asyncio.sleep_ms
+        debug = self.debug
         wlan = network.WLAN(network.STA_IF)
+
         wlan.active(True)
-        await asyncio.sleep_ms(0)
+        await sleep_ms(0)
 
         if not wlan.isconnected():
             wlan.connect(ssid, pw)
-            self.debug and print("Connecting to", ssid)
+            debug and print("Connecting to", ssid)
             while not wlan.isconnected():
-                await asyncio.sleep_ms(50)
+                await sleep_ms(50)
         return wlan
